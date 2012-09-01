0308ffa5eec5a3471324481655992fb07f5f7e0a
Jonathan Pepin <jypepin@gmail.com>
2012-09-01 09:48:20 -0400

    first commit of the almost complete profile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jypepin::Application.config.secret_token = '89716fcd14116f664b08df34ccfcc3ef0905edc5fa3345c05310d88792ae8b773b5ab5f6ff23e2d51db5dd20fe37f8f352f23225345d75f14a11a97d4a373247'
